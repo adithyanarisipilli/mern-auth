f408c7268abab037dcacf069ecda16a7a56afbe7
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-06-21 01:59:46 +0530

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,50 @@
+import { set } from "mongoose";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/backend/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-5xl text-center font-semibold my-20">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-5 rounded-lg text-lg" // Setting font size to text-lg
           style={{ fontSize: "24px", lineHeight: "32px", padding: "12px" }}
+          onChange={handleChange}
         />
         <input
           type="email"
@@ -18,6 +52,7 @@ export default function SignUp() {
           id="email"
           className="bg-slate-100 p-5 rounded-lg text-lg" // Setting font size to text-lg
           style={{ fontSize: "24px", lineHeight: "32px", padding: "12px" }}
+          onChange={handleChange}
         />
         <input
           type="password"
@@ -25,9 +60,13 @@ export default function SignUp() {
           id="password"
           className="bg-slate-100 p-5 rounded-lg text-lg" // Setting font size to text-lg
           style={{ fontSize: "24px", lineHeight: "32px", padding: "12px" }}
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white text-2xl p-5 rounded-3xl uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white text-2xl p-5 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5 text-2xl">
@@ -36,6 +75,9 @@ export default function SignUp() {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5 text-2xl">
+        {error && "Something went wrong!"}
+      </p>
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 5173, // Ensure this matches the port you are proxying to in Express
+    proxy: {
+      '/backend': {
+        target: 'http://localhost:3000',
+        secure: false,
+        changeOrigin: true, // Add this if necessary
+      },
+    },
   },
+  plugins: [react()],
 });
